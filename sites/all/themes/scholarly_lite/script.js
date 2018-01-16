665f06e7cb89e710856052d0c4f4a415f8d063ef
Asad Ali Akram <asad.ali@devtrongenesis.com>
2018-01-16 10:17:38 +0500

    Modify cource to course in js file

--- sites/all/themes/scholarly_lite/script.js
+++ sites/all/themes/scholarly_lite/script.js
@@ -3,20 +3,20 @@
 	$(document).ready(function() {
 
 			// Add clases to course menu
-    $('#block-menu-menu-cource').children().children('ul').addClass('course');
-    $('#block-menu-menu-cource .course').children('li').addClass('course-li');
-    $('#block-menu-menu-cource .course-li').children('ul').addClass('lecture');
-    $('#block-menu-menu-cource .lecture').children('li').addClass('lecture-li');
-    $('#block-menu-menu-cource .lecture-li').children('ul').addClass('video');
+    $('#block-menu-menu-course').children().children('ul').addClass('course');
+    $('#block-menu-menu-course .course').children('li').addClass('course-li');
+    $('#block-menu-menu-course .course-li').children('ul').addClass('lecture');
+    $('#block-menu-menu-course .lecture').children('li').addClass('lecture-li');
+    $('#block-menu-menu-course .lecture-li').children('ul').addClass('video');
     	// Remove href from 1st and 2nd level of course menu
-    $('#block-menu-menu-cource .course-li').children('a').removeAttr('href');
-    $('#block-menu-menu-cource .lecture-li').children('a').removeAttr('href');
+    $('#block-menu-menu-course .course-li').children('a').removeAttr('href');
+    $('#block-menu-menu-course .lecture-li').children('a').removeAttr('href');
     	// Add css of pointer to course menu list
-  	$("#block-menu-menu-cource li:has(ul)").find("a").css('cursor', 'pointer');
+  	$("#block-menu-menu-course li:has(ul)").find("a").css('cursor', 'pointer');
 	  	// Toggle Sub Nav
-  	$("#block-menu-menu-cource li:has(ul)").children("ul").hide(); // hide the li UL
+  	$("#block-menu-menu-course li:has(ul)").children("ul").hide(); // hide the li UL
   	// $("#block-menu-menu-cource li:has(ul)").find("a").removeAttr('href');
-  	$("#block-menu-menu-cource li:has(ul)").find("a").click(function() {
+  	$("#block-menu-menu-course li:has(ul)").find("a").click(function() {
     		// Add .show-subnav class to revele on click
     	$(this).toggleClass('course-active-color');
     	$(this).parent().find("ul:first").toggleClass("show-subnav");
